0388831264749b6a64e1328237e6eb10d3b76cfc
jpvalery <jpvalery@users.noreply.github.com>
2025-06-13 22:30:29 -0400

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Next Departures | Jp',
+  description: 'Come join me on my next flight',
   generator: 'v0.dev',
 }
 
