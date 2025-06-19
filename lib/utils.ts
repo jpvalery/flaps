3623c4a70d901863e811a12b50c9bdb7ede13d37
jpvalery <jpvalery@users.noreply.github.com>
2025-06-19 17:04:27 -0400

    chore: cleanup

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,3 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
-
-///////
-
 import type { Booking, Flight } from '@/types';
 
 export function emailDataFallbacks(booking: Booking) {
