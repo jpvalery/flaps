991acc0be7aaa3d6f9c238431fd47e362c9885fb
jpvalery <jpvalery@users.noreply.github.com>
2025-06-26 17:05:47 -0400

    feat: og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ import './globals.css';
 export const metadata: Metadata = {
 	title: `Next Departures | ${process.env.NEXT_PUBLIC_PILOT_NAME}`,
 	description: 'Come join me on my next flight',
+	openGraph: {
+		images: [
+			{
+				url: `https://og.jpvalery.me/api/og/flaps/${process.env.NEXT_PUBLIC_PILOT_NAME || 'Your Friendly Pilot'}`,
+				width: 1200,
+				height: 630,
+				alt: '',
+			},
+		],
+	},
 	generator: 'v0.dev',
 };
 
